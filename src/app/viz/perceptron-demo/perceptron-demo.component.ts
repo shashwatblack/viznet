91134269db338110fc713df0002e02a964158143
Shashwat Black <shashwat.black@gmail.com>
2019-04-15 10:46:01 -0500

    Added perceptron demo.

--- src/app/viz/perceptron-demo/perceptron-demo.component.ts
+++ src/app/viz/perceptron-demo/perceptron-demo.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perceptron-demo.component.scss']
 })
 export class PerceptronDemoComponent implements OnInit {
+  public form: any = {
+    input1: 10,
+    input2: 10,
+    weight1: 1,
+    weight2: 1,
+    output: 0
+  };
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.calculate();
+  }
+
+  calculate() {
+    this.form.output = this.form.input1 * this.form.weight1 + this.form.input2 * this.form.weight2;
+  }
 }
